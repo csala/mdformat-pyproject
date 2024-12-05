e98ed197f6aeb8bdc468b2188227424f4c53fdca
Joren Hammudoglu <jhammudoglu@gmail.com>
2024-12-05 15:35:06 +0100

    insert newline after `if` block

--- mdformat_pyproject/plugin.py
+++ mdformat_pyproject/plugin.py
@@ -53,6 +53,7 @@ def _parse_pyproject(pyproject_path: pathlib.Path) -> Optional[_ConfigOptions]:
     if options is not None:
         mdformat._conf._validate_keys(options, pyproject_path)
         mdformat._conf._validate_values(options, pyproject_path)
+
     return options
 
 
