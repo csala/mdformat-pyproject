b8b80311a378f3e5a53ca080fa8d71b254d5fe9f
Carles Sala <carles@pythiac.com>
2025-10-28 08:07:12 +0100

    Bump version to release v0.1.0

--- mdformat_pyproject/__init__.py
+++ mdformat_pyproject/__init__.py
@@ -1,5 +1,5 @@
 """An mdformat plugin for reading configuration from ``pyproject.toml`` files."""
 
-__version__ = "0.0.2"
+__version__ = "0.1.0"
 
 from .plugin import RENDERERS, update_mdit  # noqa: F401
